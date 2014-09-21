eebe9737d6de34261f6bec5b7b57ae973978c9e2
sdggfdg <m8911@gmx.de>
2014-09-21 15:11:54 +0200

    improvements

--- PDBReader/Source.cpp
+++ PDBReader/Source.cpp
@@ -1,3 +1,4 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <windows.h>
 #include <Dbghelp.h>
 #include <stdio.h>
@@ -19,6 +20,17 @@ WCHAR iniPath[MAX_PATH] = { 0 };
 typedef void (WINAPI *t_GetNativeSystemInfo)(LPSYSTEM_INFO lpSystemInfo);
 
 
+WCHAR * functionNames[] = {
+	L"NtUserQueryWindow",
+	L"NtUserBuildHwndList",
+	L"NtUserFindWindowEx",
+	L"NtUserInternalGetWindowText",
+	L"NtUserGetClassName"
+};
+
+DWORD_PTR functionVA[_countof(functionNames)] = {0};
+DWORD functionRVA[_countof(functionNames)] = {0};
+
 void WriteApiInIni(const WCHAR * name, DWORD address) //rva
 {
 	wsprintfW(returnBuf, L"%08X", address);
@@ -36,7 +48,13 @@ ULONG_PTR GetFunctionAddressPDB(HMODULE hMod, const WCHAR * name)
 
 	if (!SymFromNameW(GetCurrentProcess(), name, info))
 	{
-		printf("SymFromName %S returned error : %d\n", name, GetLastError());
+		DWORD err = GetLastError();
+		printf("SymFromName %S returned error : %d\n", name, err);
+
+		if (err == ERROR_MOD_NOT_FOUND)
+		{
+			printf("126 - ERROR_MOD_NOT_FOUND - You must have a working internet connection to download missing PDB files!\n\n");
+		}
 		return 0;
 	}
 
@@ -85,8 +103,9 @@ int wmain(int argc, wchar_t* argv[])
 	wsprintfW(OsId, L"%02X%02X%02X%02X%02X%02X_x86", (DWORD)osver.dwMajorVersion, (DWORD)osver.dwMinorVersion, (DWORD)osver.wServicePackMajor, (DWORD)osver.wServicePackMinor, (DWORD)osver.wProductType, (DWORD)si.wProcessorArchitecture);
 #endif
 
+	printf("OS MajorVersion %d MinorVersion %d\n", (DWORD)osver.dwMajorVersion, (DWORD)osver.dwMinorVersion);
 
-	printf("OS ID: %S\n", OsId);
+	printf("OS ID: %S\n\n", OsId);
 
 	if (!SymInitializeW(GetCurrentProcess(), symbolPath, TRUE))
 	{
@@ -95,31 +114,31 @@ int wmain(int argc, wchar_t* argv[])
 	}
 
 	HMODULE hUser = GetModuleHandleW(L"user32.dll");
+	PIMAGE_DOS_HEADER pDosUser = (PIMAGE_DOS_HEADER)hUser;
+	PIMAGE_NT_HEADERS pNtUser = (PIMAGE_NT_HEADERS)((DWORD_PTR)pDosUser + pDosUser->e_lfanew);
+
+	if (pNtUser->Signature != IMAGE_NT_SIGNATURE)
+	{
+		printf("Wrong User NT Header\n");
+		return 0;
+	}
+	wsprintfW(temp, L"%08X", pNtUser->OptionalHeader.AddressOfEntryPoint);
+	wcscat(OsId, L"_");
+	wcscat(OsId, temp);
 
 	if (hUser)
 	{
 		printf("User32 Base %p\n", hUser);
 
-		PIMAGE_DOS_HEADER pDos = (PIMAGE_DOS_HEADER)hUser;
-		PIMAGE_NT_HEADERS pNt = (PIMAGE_NT_HEADERS)((DWORD_PTR)pDos + pDos->e_lfanew);
-
-		wsprintfW(temp, L"%08X", pNt->OptionalHeader.AddressOfEntryPoint);
-		wcscat(OsId, L"_");
-		wcscat(OsId, temp);
-
-		ULONG_PTR addressNtUserQueryWindow = GetFunctionAddressPDB(hUser, L"NtUserQueryWindow");
-		ULONG_PTR addressNtUserBuildHwndList = GetFunctionAddressPDB(hUser, L"NtUserBuildHwndList");
-		ULONG_PTR addressNtUserFindWindowEx = GetFunctionAddressPDB(hUser, L"NtUserFindWindowEx");
-
-		if (addressNtUserQueryWindow)
+		for (int i = 0; i < _countof(functionNames); i++)
 		{
-			printf("Name %S RVA %08X\n", L"NtUserQueryWindow", (DWORD)(addressNtUserQueryWindow - (ULONG_PTR)hUser));
-			printf("Name %S RVA %08X\n", L"NtUserBuildHwndList", (DWORD)(addressNtUserBuildHwndList - (ULONG_PTR)hUser));
-			printf("Name %S RVA %08X\n", L"NtUserFindWindowEx", (DWORD)(addressNtUserFindWindowEx - (ULONG_PTR)hUser));
-
-			WriteApiInIni(L"NtUserQueryWindow", (DWORD)(addressNtUserQueryWindow - (ULONG_PTR)hUser));
-			WriteApiInIni(L"NtUserBuildHwndList", (DWORD)(addressNtUserBuildHwndList - (ULONG_PTR)hUser));
-			WriteApiInIni(L"NtUserFindWindowEx", (DWORD)(addressNtUserFindWindowEx - (ULONG_PTR)hUser));
+			functionVA[i] = GetFunctionAddressPDB(hUser, functionNames[i]);
+			if (functionVA[i])
+			{
+				functionRVA[i] = (DWORD)(functionVA[i] - (DWORD_PTR)hUser);
+				printf("Name %S VA %p RVA %08X\n", functionNames[i], functionVA[i], functionRVA[i]);
+				WriteApiInIni(functionNames[i], functionRVA[i]);
+			}
 		}
 	}
 
